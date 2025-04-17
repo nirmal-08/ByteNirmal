01fd84a628da555e58b92351c94e6af16765d8ff
valanirmal18 <nirmalvala18@gmail.com>
2025-04-17 18:04:26 +0530

    commited

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // 👈 Add this line (use your GitHub repo name here)
+  base: '/ByteNirmal/', // 👈 Add this line (use your GitHub repo name here)
 });
